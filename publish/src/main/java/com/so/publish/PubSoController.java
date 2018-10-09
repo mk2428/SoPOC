054002929908ae329ff4e9ad6ba158ed60073e65
Mohan K <mohanasundharam.k@accionlabs.com>
2018-10-09 16:55:12 +0530

    CRUD Operation Completed

--- publish/src/main/java/com/so/publish/PubSoController.java
+++ publish/src/main/java/com/so/publish/PubSoController.java
@@ -3,10 +3,18 @@ package com.so.publish;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -27,5 +35,26 @@ public class PubSoController {
 	public List<User> getAllPets() {
 	  return repository.findAll();
 	}
-	
+	@GetMapping("/getUser/{id}")
+	public User getUserById(@PathVariable("id") ObjectId id) {
+		return repository.findBy_id(id);
+	}
+	@PutMapping("/updateUser/{id}")
+	public void updateUserById(@PathVariable("id") ObjectId id,@Valid @RequestBody User user) {
+		user.set_id(id);
+		repository.save(user);
+	}
+		 
+	 @PostMapping("/createuser")
+	 public User createUser(@Valid @RequestBody User user) {
+		user.set_id(ObjectId.get());
+		repository.save(user);
+		return user;
+	 }
+	  
+	  @DeleteMapping("/deleteUser/{id}")
+	  public void deleteUserById(@PathVariable("id") ObjectId id) {
+		  repository.delete(repository.findBy_id(id));
+	  }
+	  
 }
